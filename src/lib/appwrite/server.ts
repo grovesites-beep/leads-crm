c1cf3705430dea47e8f40f551abd4a1c49a6faf8
Nei Espindola <nespindola@groveagency.local>
2026-03-03 09:33:21 -0300

    feat: configuracao do appwrite e dockerfile

---
+++ src/lib/appwrite/server.ts
@@ -0,0 +1,20 @@
+import { Client, Account, Databases, Storage } from 'appwrite';
+
+export const createAdminClient = async () => {
+    const client = new Client()
+        .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
+        .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!)
+        .setKey(process.env.APPWRITE_API_KEY!);
+
+    return {
+        get account() {
+            return new Account(client);
+        },
+        get databases() {
+            return new Databases(client);
+        },
+        get storage() {
+            return new Storage(client);
+        }
+    };
+};
